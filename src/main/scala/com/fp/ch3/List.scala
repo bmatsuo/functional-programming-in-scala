aefa868207f1de882ec4ed47275534a1b0a3db0d
Bryan Matsuo <bryan dot matsuo at gmail dot com>
2013-09-28 02:05:09 -0700

    [ch3] methodize

--- src/main/scala/com/fp/ch3/List.scala
+++ src/main/scala/com/fp/ch3/List.scala
@@ -2,19 +2,149 @@ package com.fp.ch3
 
 import scala.annotation.tailrec
 
-sealed trait List[+A]
+sealed trait List[+A] {
+  // Exercise 2
+  def tail: List[A] = this match {
+    case Cons(_, t) => t
+    case Nil        => throw new RuntimeException("tail of Nil is not defined")
+  }
+
+  // Exercise 3
+  // the typing appears to be important. contravariant vs covariant types.
+  // B is a 'more general' A (a superclass)
+  def setHead[B >: A](b: B): List[B] =
+    this match {
+      case Cons(_, t) => Cons(b, t)
+      case Nil        => Nil
+    }
+
+  // Exercise 4
+  @tailrec
+  final def drop(n: Int): List[A] =
+    if (n < 0) throw new RuntimeException("drop given a negative number")
+    else if (n == 0) this
+    else this match {
+      case Cons(_, t) => t.drop(n-1)
+      case Nil        => Nil
+    }
+
+  // Exercise 5
+  @tailrec
+  final def dropWhile(cont: A => Boolean): List[A] = this match {
+    case Cons(x, xs)
+      if cont(x) => xs.dropWhile(cont)
+    case _       => this
+  }
+
+  // Exercise 6: order n unlike `tail`
+  def init: List[A] = this match {
+    case Cons(a, Nil) => Nil
+      println(s"bot $this")
+      Nil
+    case Cons(a, Cons(b, t))  =>
+      println(s"rec $this")
+      val _init = Cons(a, Cons(b, t).init)
+      println(s"bld ${_init}")
+      _init
+    case Nil          => throw new RuntimeException("init of Nil is not defined")
+  }
+
+  // foldRight(List(a, b, c)), z)(f)
+  // f(a, foldRight(List(b, c), z)(f))
+  // f(a, f(b, foldRight(List(c), z)(f)))
+  // f(a, f(b, f(c, foldRight(Nil, z)(f))))
+  // f(a, f(b, f(c, z)))
+  def foldRight[B](z: B)(f: (A, B) => B): B =
+    this match {
+      case Nil        => z
+      case Cons(h, t) => f(h, t.foldRight(z)(f))
+    }
+
+  def length: Int =
+    foldRight(0)((_, b) => b + 1)
+
+  // foldLeft(List(a, b, c), z)(f)
+  // foldLeft(List(b, c), f(z, a))(f)
+  // foldLeft(List(c), f(f(z, a), b))(f)
+  // foldLeft(Nil, f(f(f(z, a), b), c))(f)
+  // f(f(f(z, a), b), c)
+  @tailrec
+  final def foldLeft[B](z: B)(f: (B, A) => B): B =
+    this match {
+      case Cons(h, t) => t.foldLeft(f(z, h))(f)
+      case Nil        => z
+    }
+
+  def lengthLeft: Int =
+    foldLeft(1)((count, _) => count + 1)
+
+  def reverse: List[A] =
+    foldLeft(Nil: List[A])((rev, h) => Cons(h, rev))
+
+  def concat[B >: A](bs: List[B]): List[B] =
+    foldRight(bs)(Cons(_, _))
+
+  def append[B >: A](b: B): List[B] =
+    concat(List(b))
+
+  def foldRight2[B](z: B)(f: (A, B) => B): B =
+    reverse.foldLeft(z)((b, a) => f(a, b))
+
+  def map[B](f: A => B): List[B] =
+    foldRight(Nil: List[B])((a, bs) => Cons(f(a), bs))
+
+  def filter(f: A => Boolean): List[A] =
+    foldRight(Nil: List[A]) { (a, as) =>
+      if (f(a)) Cons(a, as)
+      else as
+    }
+
+  def flatMap[B](f: A => List[B]): List[B] =
+    List.flatten[B](this.map(f))
+
+  def filter2(f: A => Boolean): List[A] =
+    flatMap { a => if (f(a)) List(a) else Nil }
+
+  def zip[B, C](lis2: List[B])(f: (A, B) => C): List[C] =
+    (this, lis2) match {
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons(f(h1, h2), t1.zip(t2)(f))
+      case _                            => Nil
+    }
+
+  def hasSubsequence[B >: A](sub: List[B]): Boolean = {
+    @tailrec
+    def startsWith(lis: List[A], sub: List[B]): Boolean =
+      (lis, sub) match {
+        case (Cons(h, t), Cons(hsub, tsub))
+          if h == hsub => startsWith(t, tsub)
+        case (_, Nil)  => true
+        case (Nil, _)  => false
+        case _         => false
+      }
+
+    @tailrec
+    def hasSub(lis: List[A]): Boolean =
+      if (startsWith(lis, sub)) true
+      else lis match {
+        case Cons(_, t) => hasSub(t)
+        case Nil        => false
+      }
+
+    hasSub(this)
+  }
+}
 case object Nil extends List[Nothing]
-case class Cons[+A](head: A, tail: List[A]) extends List[A]
+case class Cons[+A](h: A, t: List[A]) extends List[A]
 
 object List {
   def sum(ints: List[Int]): Int = ints match {
-    case Nil => 0
     case Cons(x, xs) => x + sum(xs)
+    case Nil         => 0
   }
 
   def product(ds: List[Double]): Double = ds match {
-    case Nil => 1
     case Cons(x, xs) => x * product(xs)
+    case Nil         => 1
   }
 
   // This is kind of bullshit because it bootstraps on Seq
@@ -24,164 +154,38 @@ object List {
 
   // Exercise 1: x == 3
   val x = List(1, 2, 3, 4, 5) match {
-    case Cons(a, Cons(2, Cons(4, _))) => a
-    case Nil => 42
+    case Cons(a, Cons(2, Cons(4, _)))          => a
+    case Nil                                   => 42
     case Cons(a, Cons(b, Cons(3, Cons(4, _)))) => a + b
-    case Cons(h, t) => h + sum(t)
-    case _ => 101
+    case Cons(h, t)                            => h + sum(t)
+    case _                                     => 101
   }
-
-  // Exercise 2
-  def tail[A](lis: List[A]): List[A] = lis match {
-    case Nil => throw new RuntimeException("tail of Nil is not defined")
-    case Cons(_, t) => t
-  }
-
-  // Exercise 3
-  // the typing appears to be important. contravariant vs covariant types.
-  // B is a 'more general' A (a superclass)
-  def setHead[A](lis: List[A], a: A): List[A] = lis match {
-    case Nil => throw new RuntimeException("setHead of Nil is not defined")
-    case Cons(_, t) => Cons(a, t)
-  }
-
-  // Exercise 4
-  @tailrec
-  def drop[A](as: List[A], n: Int): List[A] =
-    if (n < 0)
-      throw new RuntimeException("drop given a negative number")
-    else if (n == 0)
-      as
-    else as match {
-      case Nil => throw new RuntimeException("drop")
-      case Cons(_, as) => drop(as, n-1)
-    }
-
-  // Exercise 5
-  @tailrec
-  def dropWhile[A](as: List[A], cont: A => Boolean): List[A] = as match {
-    case Cons(x, xs) if cont(x) => dropWhile(xs, cont)
-    case _ => as
-  }
-
-  // Exercise 6: order n unlike `tail`
-  def init[A](as: List[A]): List[A] = as match {
-    case Nil => throw new RuntimeException("init of Nil is not defined")
-    case Cons(a, Nil) => Nil
-    case Cons(a, as) => Cons(a, init(as))
-  }
-
-  // foldLeft(Cons(a, Cons(b, Cons(c, Nil))), z)(f)
-  // f(a, foldLeft(Cons(b, Cons(c, Nil)), z)(f))
-  // f(a, f(b, foldLeft(Cons(c, Nil), z)(f)))
-  // f(a, f(b, f(c, foldLeft(Nil, z)(f))))
-  // f(a, f(b, f(c, z)))
-  def foldRight[A, B](lis: List[A], z: B)(f: (A, B) => B): B =
-    lis match {
-      case Nil => z
-      case Cons(h, t) => f(h, foldRight(t, z)(f))
-    }
 
   def sum2(lis: List[Int]): Int =
-    foldRight(lis, 0)(_ + _)
+    lis.foldRight(0)(_ + _)
 
   def product2(lis: List[Double]): Double =
-    foldRight(lis, 1.0)(_ * _)
-
-  def length[A](lis: List[A]): Int =
-    foldRight(lis, 0)((_, b) => b + 1)
-
-  // foldLeft(Cons(a, Cons(b, Cons(c, Nil))), z)(f)
-  // foldLeft(Cons(b, Cons(c, Nil)), f(z, a))(f)
-  // foldLeft(Cons(c, Nil), f(f(z, a), b))(f)
-  // foldLeft(Nil, f(f(f(z, a), b), c))(f)
-  // f(f(f(z, a), b), c)
-  @tailrec
-  def foldLeft[A, B](lis: List[A], z: B)(f: (B, A) => B): B =
-    lis match {
-      case Nil => z
-      case Cons(h, t) => foldLeft(t, f(z, h))(f)
-    }
+    lis.foldRight(1.0)(_ * _)
 
   def sumLeft(lis: List[Int]): Int =
-    foldLeft(lis, 0)(_ + _)
+    lis.foldLeft[Int](0)(_ + _)
 
   def productLeft(lis: List[Double]): Double =
-    foldLeft(lis, 1.0)(_ * _)
-
-  def lengthLeft[A](lis: List[A]): Int =
-    foldLeft(lis, 1)((count, _) => count + 1)
-
-  def reverse[A](lis: List[A]): List[A] =
-    foldLeft(lis, Nil: List[A])((rev, h) => Cons(h, rev))
-
-  def concat[A](lis1: List[A], lis2: List[A]): List[A] =
-    foldRight(lis1, lis2)(Cons(_, _))
-
-  def append[A](lis: List[A], a: A): List[A] =
-    concat(lis, List(a))
+    lis.foldLeft[Double](1.0)(_ * _)
 
   def flatten[A](lis: List[List[A]]): List[A] =
-    foldRight(lis, Nil: List[A])(concat)
-
-  def foldRight2[A, B](lis: List[A], z: B)(f: (A, B) => B): B =
-    foldLeft(reverse(lis), z)((b, a) => f(a, b))
+    lis.foldRight(Nil: List[A])(_ concat _)
 
   def incrAll(lis: List[Int]): List[Int] =
-    foldRight(lis, Nil: List[Int])((i, t) => Cons(i + 1, t))
+    lis.foldRight(Nil: List[Int])((i, t) => Cons(i + 1, t))
 
   def doubleStrings(lis: List[Double]): List[String] =
-    foldRight(lis, Nil: List[String])((d, t) => Cons(d.toString, t))
-
-  def map[A, B](as: List[A])(f: A => B): List[B] =
-    foldRight(as, Nil: List[B])((a, bs) => Cons(f(a), bs))
-
-  def filter[A](lis: List[A])(f: A => Boolean): List[A] =
-    foldRight(lis, Nil: List[A]) { (a, as) =>
-      if (f(a)) Cons(a, as)
-      else as
-    }
-
-  def flatMap[A, B](lis: List[A])(f: A => List[B]): List[B] =
-    flatten(map(lis)(f))
-
-  def filter2[A](lis: List[A])(f: A => Boolean): List[A] =
-    flatMap(lis) { a => if (f(a)) List(a) else Nil }
+    lis.foldRight(Nil: List[String])((d, t) => Cons[String](s"$d", t))
 
   def zipAdd(lis1: List[Int], lis2: List[Int]): List[Int] =
     (lis1, lis2) match {
-      case (_, Nil) => lis1
-      case (Nil, _) => lis2
       case (Cons(h1, t1), Cons(h2, t2)) => Cons(h1+h2, zipAdd(t1, t2))
+      case (_, Nil)                     => Nil
+      case (Nil, _)                     => Nil
     }
-
-  def zip[A, B](lis1: List[A], lis2: List[A])(f: (A, A) => B): List[B] =
-    (lis1, lis2) match {
-      case (Nil, Nil) => Nil
-      case (_, Nil) => throw new RuntimeException("zip")
-      case (Nil, _) => throw new RuntimeException("shit")
-      case (Cons(h1, t1), Cons(h2, t2)) => Cons(f(h1, h2), zip(lis1, lis2)(f))
-    }
-
-  def hasSubsequence[A](lis: List[A], sub: List[A]): Boolean = {
-    @tailrec
-    def startsWith(lis: List[A], sub: List[A]): Boolean =
-      (lis, sub) match {
-        case (_, Nil) => true
-        case (Nil, _) => false
-        case (Cons(hlis, tlis), Cons(hsub, tsub)) if hlis == hsub => startsWith(tlis, tsub)
-        case _ => false
-      }
-
-    @tailrec
-    def hasSub(lis: List[A]): Boolean =
-      if (startsWith(lis, sub)) true
-      else lis match {
-        case Nil => false
-        case Cons(_, tlis) => hasSub(tlis)
-      }
-
-    hasSub(lis)
-  }
-
 }
--- src/main/scala/com/fp/ch3/Tree.scala
+++ src/main/scala/com/fp/ch3/Tree.scala
@@ -1,71 +1,59 @@
 package com.fp.ch3
 
-sealed trait Tree[+A]
+sealed trait Tree[+A] {
+  import Tree._
+
+  def size: Int =
+    this match {
+      case Branch(left, right) => 1 + left.size + right.size
+      case Leaf(_)             => 1
+    }
+
+  def depth: Int =
+    this match {
+      case Branch(left, right) =>  max(left.depth, right.depth) + 1
+      case Leaf(_)             => 0
+    }
+
+  def map[B](f: A => B): Tree[B] =
+    this match {
+      case Branch(left, right) => Branch(left.map(f), right.map(f))
+      case Leaf(a)             => Leaf(f(a))
+    }
+
+  def fold[B](g: A => B, f: (B, B) => B): B =
+    this match {
+      case Branch(left, right) => f(left.fold(g, f), right.fold(g, f))
+      case Leaf(a)             => g(a)
+    }
+
+  def sizeFold: Int =
+    fold[Int](
+      _ => 1,
+      1 + _ + _)
+
+  def depthFold: Int =
+    fold[Int](
+      _ => 0,
+      max(_, _) + 1)
+
+  def mapFold[B](f: A => B): Tree[B] =
+    fold[Tree[B]](a => Leaf(f(a)), Branch.apply)
+}
+
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
-  def size[A](t: Tree[A]): Int =
-    t match {
-      case Leaf(_) => 1
-      case Branch(left, right) => 1 + size(left) + size(right)
-    }
+  private def max2(x: Int, y: Int): Int = if (x > y) x else y
+  private def max(x: Int, xs: Int*): Int = xs.foldLeft(x)(max2)
 
   def maxInt(t: Tree[Int]): Int =
     t match {
-      case Leaf(x) => x
-      case Branch(left, right) => {
-        val maxLeft = maxInt(left)
-        val maxRight = maxInt(right)
-        if (maxLeft > maxRight) maxLeft else maxRight
-      }
+      case Branch(left, right) => max(maxInt(left), maxInt(right))
+      case Leaf(x)             => x
     }
-
-  def depth[A](t: Tree[A]): Int =
-    t match {
-      case Leaf(_) => 0
-      case Branch(left, right) => {
-        val depthLeft = depth(left)
-        val depthRight = depth(right)
-        if (depthLeft > depthRight) depthLeft + 1 else depthRight + 1
-      }
-    }
-
-  def map[A, B](t: Tree[A])(f: A => B): Tree[B] =
-    t match {
-      case Leaf(a) => Leaf(f(a))
-      case Branch(left, right) => Branch(map(left)(f), map(right)(f))
-    }
-
-  def fold[A, B](t: Tree[A], z: B)(f: (Tree[A], B) => B): B =
-    t match {
-      case Leaf(a) => f(Leaf(a), z)
-      case Branch(left, right) => f(Branch(left, right), fold(right, f(left, fold(left, z)(f)))(f))
-    }
-
-  def fold2[A, B](t: Tree[A])(g: A => B, f: (B, B) => B): B =
-    t match {
-      case Leaf(a) => g(a)
-      case Branch(left, right) => f(fold2(left)(g, f), fold2(right)(g, f))
-    }
-
-  def sizeFold(t: Tree[Int]): Int =
-    fold2[Int, Int](t)(
-      _ => 1,
-      1 + _ + _)
 
   def maxIntFold(t: Tree[Int]): Int =
-    fold2(t)(
-      x => x,
-      (x1: Int, x2: Int) => if (x1 > x2) x1 else x2)
-
-  def depthFold[A](t: Tree[A]): Int =
-    fold2[A, Int](t)(
-      _ => 0,
-      (d1, d2) => if (d1 > d2) d1 + 1 else d2 + 1)
-
-  def mapFold[A, B](t: Tree[A])(f: A => B): Tree[B] =
-    fold2[A,Tree[B]](t)(
-      a => Leaf(f(a)),
-      (left, right) => Branch(left, right))
+    t.fold[Int](identity, max(_, _))
 }
