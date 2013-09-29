62ee133f35563b7f058817cbf41aa2b8b1ea3570
Bryan Matsuo <bryan dot matsuo at gmail dot com>
2013-09-29 04:32:23 -0700

    [ch6] exercises and some tests

--- src/main/scala/com/fp/ch6/RNG.scala
+++ src/main/scala/com/fp/ch6/RNG.scala
@@ -1,7 +1,49 @@
 package com.fp.ch6
 
+import com.fp.ch5._
+
+
 trait RNG {
   def nextInt: (Int, RNG)
+
+  def positiveInt: (Int, RNG) =
+    nextInt match {
+      case (x, r) if x == Int.MinValue  => r.positiveInt
+      case (x, r) if x == 0             => r.positiveInt
+      case (x, r)                       => (math.abs(x), r)
+    }
+
+  def postiveEven: (Int, RNG) = RNG.positiveEven(this)
+
+  def double: (Double, RNG) = RNG.double(this)
+
+  def intDouble: ((Int, Double), RNG) = {
+    val (n, r) = nextInt
+    val (x, r2) = r.double
+    ((n, x), r2)
+  }
+
+  def doubleInt: ((Double, Int), RNG) = {
+    val (x, r) = double
+    val (n, r2) = r.nextInt
+    ((x, n), r2)
+  }
+
+  def doubleDoubleDouble: ((Double, Double, Double), RNG) = {
+    val (x, r) = double
+    val (x2, r2) = r.double
+    val (x3, r3) = r2.double
+    ((x, x2, x3), r3)
+  }
+
+  def ints(count: Int): (List[Int], RNG) =
+    if (count <= 0)
+      (Nil, this)
+    else {
+      val (x, r) = nextInt
+      val (xs, r2) = r.ints(count - 1)
+      (x :: xs, r2)
+    }
 }
 
 object RNG {
@@ -11,4 +53,69 @@ object RNG {
       ((seed >>> 16).asInstanceOf[Int], simple(seed2))
     }
   }
+
+  type Rand[+A] = State[RNG, A]
+
+  val int: Rand[Int] =
+    State(_.nextInt)
+
+  val positiveInt: Rand[Int] =
+    State(_.positiveInt)
+
+  def unit[A](a: A): Rand[A] =
+    State((a, _))
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    s.map(f)
+
+  val positiveEven: Rand[Int] =
+    map(positiveInt)(i => i - i % 2)
+
+  val double: Rand[Double] =
+    map(positiveInt)(n => (n.toDouble - 1) / Int.MaxValue)
+
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    ra.map2(rb)(f)
+
+  def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] =
+    ra.map2(rb)((_, _))
+
+  val intDouble: Rand[(Int, Double)] = both(int, double)
+
+  val doubleInt: Rand[(Double, Int)] = both(double, int)
+
+  val doubleDoubleDouble: Rand[(Double, Double, Double)] =
+    flatMap(double)(x1 =>
+      flatMap(double)(x2 =>
+        map(double)(x3 => (x1, x2, x3))))
+
+  def sequence[A](ras: List[Rand[A]]): Rand[List[A]] =
+    State.sequence(ras)
+
+  def ints(count: Int): Rand[List[Int]] =
+    sequence(List.fill(count)(int))
+
+  def flatMap[A, B](ra: Rand[A])(g: A => Rand[B]): Rand[B] =
+    ra.flatMap(g)
+
+  def positiveLessThan(n: Int): Rand[Int] =
+    flatMap(positiveInt) { i =>
+      val mod = i % n
+      if (i + (n - 1) - mod > 0) unit(mod) else positiveLessThan(n)
+    }
+
+  def mapFlat[A, B](s: Rand[A])(f: A => B): Rand[B] = flatMap(s)(a => unit(f(a)))
+
+  def map2Flat[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra)(a => flatMap(rb)(b => unit(f(a, b))))
+
+  def map2For[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    for {
+      a <- ra
+      b <- rb
+    } yield f(a, b)
+
+  val rollDie: Rand[Int] = positiveLessThan(6)
+
+  val rollDieCorrect: Rand[Int] = map(positiveLessThan(6))(_ + 1)
 }
---
+++ src/test/scala/Ch6.scala
@@ -0,0 +1,102 @@
+import org.scalatest.FunSuite
+import org.scalatest.Matchers
+import com.fp.ch5._
+import com.fp.ch6._
+
+class Ch6 extends FunSuite with Matchers {
+  import RNG._
+
+  test("x.x: simple algorithm check") {
+    def sample(n: Int, seed: Long): Stream[Int] = Stream
+      .unfold(simple(seed))(r => Some(r.nextInt))
+      .take(n)
+
+    sample(3, 42).toList should be (List(0, 16159453, -1281479697))
+  }
+
+  test("6.1: postiveInt") {
+    def sample(n: Int, seed: Long): Stream[Int] = Stream
+      .unfold(simple(seed))(r => Some(r.positiveInt))
+      .take(n)
+
+    sample(2, 42).toList should be (List(16159453, 1281479697))
+    Range(1, 100).forall(sample(1000, _).forAll(_ > 0)) should be (true)
+  }
+
+  test("6.2: double") {
+    def sample(n: Int, seed: Long): Stream[Double] = Stream
+      .unfold(simple(seed))(r => Some(r.double))
+      .take(n)
+
+    Range(1, 100).forall(sample(1000, _).forAll(x => 1.0 > x && x >= 0.0)) should be (true)
+  }
+
+  test("6.8: flatMap and postiveLessThan") {
+    def sample(n: Int, lessThan: Int, seed: Long): Stream[Int] = Stream
+      .unfold(simple(seed))(r => Some(RNG.positiveLessThan(lessThan)(r)))
+      .take(n)
+
+    val max = 1281479697
+    Range(1, 100).forall(sample(1000, max, _).forAll(x => max > x && x > 0)) should be (true)
+  }
+
+  test("x.x: reproducible errors") {
+    rollDie(simple(5)) should be (0, _: RNG)
+  }
+
+  sealed trait Input
+
+  case object Coin extends Input
+  case object Turn extends Input
+
+  case class Machine(locked: Boolean, candies: Int, coins: Int) {
+    def simulate(input: Input): Machine =
+      if (candies <= 0) this
+      else (input, locked) match {
+        case (Turn, true)  => this
+        case (Turn, false) => Machine(true, candies-1, coins)
+        case (Coin, false) => this
+        case (Coin, true)  => Machine(false, candies, coins+1)
+      }
+  }
+
+  object Machine {
+    type MachineState = State[Machine, (Int, Int)]
+
+    private def step(input: Input): State[Machine, Unit] =
+      State.modify[Machine](_.simulate(input))
+
+    def simulate(inputs: List[Input]): State[Machine, (Int, Int)] =
+      State(m => {
+        val m1 = inputs
+          .foldLeft[State[Machine, Unit]](State.nop)((s, in) => for {
+            _ <- s
+            _ <- step(in)
+          } yield ())
+          .apply(m)._2
+
+        ((m1.coins, m1.candies), m1)
+      })
+  }
+
+  test("6.11: simulate") {
+    {
+      val m1 = Machine(true, 5, 10)
+      val inputs = List(
+        Coin, Turn,
+        Coin, Turn,
+        Coin, Turn,
+        Coin, Turn)
+      Machine.simulate(inputs)(m1) should be ((14, 1), _: Machine)
+    }
+    {
+      val m1 = Machine(true, 5, 10)
+      val inputs = List(
+        Coin, Coin, Turn, Turn,
+        Coin, Coin, Turn, Turn,
+        Coin, Coin, Turn, Turn,
+        Coin, Coin, Turn, Turn)
+      Machine.simulate(inputs)(m1) should be ((14, 1), _: Machine)
+    }
+  }
+}
